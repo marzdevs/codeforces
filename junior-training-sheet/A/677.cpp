8e7227c008a44b93f32dbe02e848a37412f8fdae
Marzdevs <alzokarimarwa@gmail.com>
2023-10-07 01:08:03 -0500

    ONE DONE

---
+++ junior-training-sheet/A/677.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, h, x, ans;
+
+int main()
+{
+    cin >> n >> h;
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> x;
+        if (x > h)
+            ans++;
+        ans++;
+    }
+    cout << ans << "\n";
+}
