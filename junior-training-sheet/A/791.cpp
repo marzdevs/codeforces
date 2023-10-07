f0f0b0545378364b9f87eaa93e0c75e5e7df021e
Marzdevs <alzokarimarwa@gmail.com>
2023-10-07 01:52:01 -0500

    done

--- junior-training-sheet/A/791.cpp
+++ junior-training-sheet/A/791.cpp
@@ -2,8 +2,17 @@
 #include <algorithm>
 using namespace std;
 
-int n, h, x, ans;
+int a, b;
 
 int main()
 {
+    cin >> a >> b;
+    int count = 0;
+    while (a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        count++;
+    }
+    cout << count << "\n";
 }
